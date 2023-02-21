fa169370b051d9e6bec226004aef7d77ea32699a
shenao-zhang <82402331+shenao-zhang@users.noreply.github.com>
2023-02-21 11:39:39 +0800

    grad clip

--- mbrl/models/one_dim_tr_model.py
+++ mbrl/models/one_dim_tr_model.py
@@ -14,6 +14,7 @@ import mbrl.util.math
 
 from .model import Ensemble, Model
 from collections import deque
+import torch.nn.utils as utils
 
 MODEL_LOG_FORMAT = [
     ("train_iteration", "I", "int"),
@@ -215,7 +216,9 @@ class OneDTransitionRewardModel(Model):
         obs = torch.from_numpy(obs).cpu().float()
 
         done = False
+        count = 0
         while not done:
+            count += 1
             actions = agent.act(obs.cpu().float())
             (
                 next_observs,
@@ -233,7 +236,7 @@ class OneDTransitionRewardModel(Model):
             obs, reward, done, model_state = next_observs, rewards, dones, next_model_state
             reward_history.append(reward)
             log_probs.append(log_p)
-
+        print(count, ", rolloutlen")
         R = 0
         model_value_loss = []
         returns = deque()
@@ -248,17 +251,12 @@ class OneDTransitionRewardModel(Model):
 
         for log_prob, R in zip(log_probs, returns):
             model_value_loss.append(-log_prob * R.detach())
-        model_value_loss = torch.cat(model_value_loss).sum() * 0.02
+        model_value_loss = torch.cat(model_value_loss).mean() * 0.02
 
         self.train()
         optimizer.zero_grad()
         model_value_loss.backward()
-        if meta is not None:
-            with torch.no_grad():
-                grad_norm = 0.0
-                for p in list(filter(lambda p: p.grad is not None, self.parameters())):
-                    grad_norm += p.grad.data.norm(2).item() ** 2
-                meta["grad_norm"] = grad_norm
+        utils.clip_grad_norm(self.model.parameters(), 40)
         optimizer.step()
         return loss + model_value_loss.item(), meta
 
